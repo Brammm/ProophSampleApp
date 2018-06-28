6a48a84889c1995385fc5f9d2bf040bdd0ab2418
Bram Van der Sype <bram.vandersype@gmail.com>
2018-06-28 17:19:02 +0200

    Configure command bus

--- config/prooph.php
+++ config/prooph.php
@@ -4,7 +4,24 @@ declare(strict_types=1);
 
 namespace {
 
-    return [
+    use Prooph\ServiceBus\CommandBus;
+    use Prooph\ServiceBus\Plugin\Router\CommandRouter;
+    use Prooph\ServiceBus\Plugin\ServiceLocatorPlugin;
+    use Psr\Container\ContainerInterface;
+    use Todo\Domain\User\RegisterUser;
+    use Todo\Domain\User\RegisterUserCommandHandler;
 
+    return [
+        CommandBus::class => function(ContainerInterface $container) {
+            $router = new CommandRouter([
+                RegisterUser::class => RegisterUserCommandHandler::class,
+            ]);
+
+            $commandBus = new CommandBus();
+            (new ServiceLocatorPlugin($container))->attachToMessageBus($commandBus);
+            $router->attachToMessageBus($commandBus);
+
+            return $commandBus;
+        },
     ];
 }
