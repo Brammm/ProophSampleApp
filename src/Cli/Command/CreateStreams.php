158b520e237ccef0d822ef5952775015fa164856
Bram Van der Sype <bram.vandersype@gmail.com>
2018-07-03 16:14:00 +0200

    Fix todo projector to bootstrap tables better

--- src/Cli/Command/CreateStreams.php
+++ src/Cli/Command/CreateStreams.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Todo\Cli\Command;
 
 use ArrayIterator;
+use PDO;
 use Prooph\EventStore\EventStore;
 use Prooph\EventStore\Exception\StreamExistsAlready;
 use Prooph\EventStore\Stream;
@@ -26,13 +27,22 @@ final class CreateStreams
         'user-stream',
     ];
 
-    public function __construct(EventStore $eventStore)
+    /**
+     * @var PDO
+     */
+    private $pdo;
+
+    public function __construct(PDO $pdo, EventStore $eventStore)
     {
         $this->eventStore = $eventStore;
+        $this->pdo = $pdo;
     }
 
     public function __invoke(OutputInterface $output): void
     {
+        $this->pdo->exec(file_get_contents(__DIR__ . '/../../../vendor/prooph/pdo-event-store/scripts/mysql/01_event_streams_table.sql'));
+        $this->pdo->exec(file_get_contents(__DIR__ . '/../../../vendor/prooph/pdo-event-store/scripts/mysql/02_projections_table.sql'));
+
         foreach ($this->streams as $stream) {
             try {
                 $this->eventStore->create(new Stream(new StreamName($stream), new ArrayIterator()));
